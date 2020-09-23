8c8eb1136b5265356ba06694a1ad33928c4bb0de
Jennifer Liao <38355986+jliao13@users.noreply.github.com>
2020-09-23 10:31:08 -0400

    initial heroku setup and files

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn anti_ux.wsgi
---
+++ project/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls import url
+from django.conf.urls.static import static
+from project import views
+
+urlpatterns = [
+    url(r'^$', views.HomePageView.as_view()),
+    url(r'^form/$', views.FormPageView.as_view()), # Add this /about/ route
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
